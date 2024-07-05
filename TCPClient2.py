cda1f263f115a6079682790eba42ac1e8ca940b0
Haruta-Yamanaka <166776002+Haruta-Yamanaka@users.noreply.github.com>
2024-07-05 11:52:56 +0900

    Add files via upload

--- TCPClient2.py
+++ TCPClient2.py
@@ -3,106 +3,112 @@ import pickle
 import threading
 from TCPServer import Cards,Card
 
-# TCPクライアントの設定
-HOST = 'localhost'
-PORT = 12345
-BUFFER_SIZE = 4096
-cards = None
-name = None
-isMyTurn = True
+class Client:
+    # TCPクライアントの設定
+    def __init__(self):
+            
+        self.HOST = 'localhost'
+        self.PORT = 12345
+        self.BUFFER_SIZE = 4096
+        self.client_socket = None
+        self.cards = None
+        self.name = None
+        self.isMyTurn = True
 
 
-def setUp(client_socket):
-    global cards,name
-    isFinish = False
-    try:#カード、名前の順に送られる
-        while not isFinish:
-            # サーバーからのデータを受信して表示
-            data = client_socket.recv(BUFFER_SIZE)
-            received_object = pickle.loads(data)
-            if not data:
-                break
-            if cards is None:
-                cards = received_object
-                cards.showDeck()
-            elif name is None:
-                name = received_object
-                print(f"あなたは{name}です")
-                isFinish = True
+    def setUp(self):
+        self.isFinish = False
+        try:#カード、名前の順に送られる
+            while not self.isFinish:
+                # サーバーからのデータを受信して表示
+                data = self.client_socket.recv(self.BUFFER_SIZE)
+                received_object = pickle.loads(data)
+                if not data:
+                    break
+                if self.cards is None:
+                    self.cards = received_object
+                    self.cards.showDeck()
+                elif self.name is None:
+                    self.name = received_object
+                    print(f"あなたは{self.name}です")
+                    self.isFinish = True
 
-    except KeyboardInterrupt:
-        print("\n[*] Client is shutting down.")
-        client_socket.close()
+        except KeyboardInterrupt:
+            print("\n[*] Client is shutting down.")
+            self.client_socket.close()
+
+    def TurnTask(self):
+        receiveThread = threading.Thread(target = self.receive_handler, args= (self.client_socket,), daemon= True)
+        receiveThread.start()
+        if self.isMyTurn:
+            inputThread = threading.Thread(target = self.input_handler, args= (self.client_socket,), daemon= True)
+            inputThread.start()
 
 
-def input_handler(client_socket):
-    your_input = input(">>>"); #(1)
-    client_socket.send(your_input.encode("UTF-8")); #(2)
+    def input_handler(self,client_socket):
+        your_input = input(">>>"); #(1)
+        client_socket.send(your_input.encode("UTF-8")); #(2)
 
 
-def receive_handler(client_socket):
-    global BUFFER_SIZE, isMyTurn
-    str = type("str")
-    while True:
-        try:
-            data = client_socket.recv(BUFFER_SIZE); #(3)
-            received_object = pickle.loads(data)
-            if(type(received_object) == str):
-                if(received_object =="Your Turn!"):
-                    isMyTurn = True
-                    print("あなたの番です。")
-                elif (received_object == "Turn End"):
-                    isMyTurn = False
-                    print("ターン終了です。")
+    def receive_handler(self,client_socket):
+        str = type("str")
+        while True:
+            try:
+                data = client_socket.recv(self.BUFFER_SIZE); #(3)
+                received_object = pickle.loads(data)
+                if(type(received_object) == str):
+                    if(received_object =="Your Turn!"):
+                        self.isMyTurn = True
+                        print("あなたの番です。")
+                    elif (received_object == "Turn End"):
+                        self.isMyTurn = False
+                        print("ターン終了です。")
+                    else:
+                        print(received_object)
                 else:
-                    print(received_object)
-            else:
-                cards = received_object
-                cards.showDeck()
-                print(getCards())
-        except Exception as e:
-            continue
+                    self.cards = received_object
+                    self.cards.showDeck()
+            except Exception as e:
+                continue
 
 
-def revealCard(client_socket):#カードをめくる処理
-    
-    #クリックしたらっていう条件分岐を作ってほしい。
-    #以下のコードはクリックした後の処理である。
+    def revealCard(self):#カードをめくる処理
+        #クリックしたらっていう条件分岐を作ってほしい。
+        #以下のコードはクリックした後の処理である。
 
-    tupple = (1,1) #ここにクリックしたカードから返されたタプルを代入したい
-    command = getCommand(tupple)
-    sendCommand(command,client_socket)
+        tupple = (1,1) #ここにクリックしたカードから返されたタプルを代入したい
+        command = self.getCommand(tupple)
+        self.sendCommand(command,self.client_socket)
 
 
-def getCards():
-    global cards
-    for card in cards:
-        return card
-    
-def sendCommand(command,client_socket):
-    client_socket.send(command.encode("UTF-8"))
+    def getCards(self):
+        for card in self.cards:
+            return card
+        
+    def sendCommand(self,command,client_socket):
+        client_socket.send(command.encode("UTF-8"))
 
 
 
-def getCommand(tupple): #選んだインデックスをサーバーに送るコマンドに変換
-    tup1 = (2,1)
-    str = str(tupple[0])+","+str(tupple[1])
-    return str
+    def getCommand(self,tupple): #選んだインデックスをサーバーに送るコマンドに変換
+        tupple = tupple
+        str = str(tupple[0])+","+str(tupple[1])
+        return str
 
-
+    def socketSetup(self):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect((self.HOST, self.PORT))
+        print(f"[*] Connected to {self.HOST}:{self.PORT}")
 
 #グローバル変数の設定
 
 
 # サーバーに接続
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((HOST, PORT))
-print(f"[*] Connected to {HOST}:{PORT}")
 
-setUp(client_socket)
+
+cl = Client()
+cl.socketSetup()
+
+cl.setUp()
 while True:
-    receiveThread = threading.Thread(target = receive_handler, args= (client_socket,), daemon= True)
-    receiveThread.start()
-    if isMyTurn:
-        inputThread = threading.Thread(target = input_handler, args= (client_socket,), daemon= True)
-        inputThread.start()
+    cl.TurnTask()
